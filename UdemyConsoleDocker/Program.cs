f412d12b2c01501618f21d927eb97fe145a945db
umutSahinkaya <umutsahinkaya1@gmail.com>
2023-09-13 10:40:00 +0300

    Add project files.

---
+++ UdemyConsoleDocker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UdemyConsoleDocker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i < 10000)
+            {
+                Console.WriteLine("Hello World!"+i);
+                i++;
+                System.Threading.Thread.Sleep(1000);
+
+            }
+        }
+    }
+}
---
+++ UdemyDocker.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyDocker.WebApp.Models;
+
+namespace UdemyDocker.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IFileProvider _fileProvider;
+        public HomeController(ILogger<HomeController> logger, IFileProvider fileProvider)
+        {
+            _logger = logger;
+            _fileProvider = fileProvider;
+        }
+        public IActionResult ImageShow()
+        {
+            var images = _fileProvider.GetDirectoryContents("wwwroot/images").ToList().Select(x => x.Name);
+
+            return View(images);
+        }
+        [HttpPost]
+        public IActionResult ImageShow(string name)
+        {
+            var file = _fileProvider.GetDirectoryContents("wwwroot/images").ToList().First(x => x.Name == name);
+            System.IO.File.Delete(file.PhysicalPath);
+            return RedirectToAction("ImageShow");
+        }
+        public IActionResult ImageSave()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> ImageSave(IFormFile imageFile)
+        {
+            if (imageFile != null && imageFile.Length > 0)
+            {
+                //resim1.jpg Mutlaka kullanıcıdan alınan resimlere bir isim tanımlamak gerek!!!
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
+                var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", fileName);
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await imageFile.CopyToAsync(stream);
+                }
+
+            }
+            return View();
+        }
+        #region Index,Privacy,Error
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        #endregion
+    }
+}
